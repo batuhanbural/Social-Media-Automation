0d35e15d0e4f679bd6109f7aeb76f1809c7a31f0
batuhanbural <60934501+batuhanbural@users.noreply.github.com>
2020-12-08 16:12:43 +0300

    v1.0.2

--- gtts_gstt.py
+++ gtts_gstt.py
@@ -1,7 +1,6 @@
 import os
 import random
 import webbrowser
-from datetime import datetime
 
 import speech_recognition as sr
 from gtts import gTTS
@@ -56,25 +55,6 @@ def speech_to_text(ask=False, lang="en"):
 def search_on_web(querry_line):
     url = "https://www.google.com/search?q=" + querry_line
     webbrowser.get().open(url)
-
-
-def response(voice: str) -> object:
-    if voice == "nasılsın":
-        text_to_speech(text="Ben iyiyim sen nasılsın?", lang="tr")
-    elif voice == "güzel miyim":
-        text_to_speech(text="Yok sen yakışıklısın Batu", lang="tr")
-    elif voice == "saat kaç":
-        text_to_speech(text=datetime.now().strftime("%H:%M:%S"), lang="tr")
-    elif voice == "arama yap":
-        search = speech_to_text("ne aramak istiyorsun")
-        url = "https://www.google.com/search?q=" + search
-        webbrowser.get().open(url)
-        text_to_speech(text=search + " için bulduklarım:", lang="tr")
-    elif voice == "sor":
-        text_to_speech(text="Ne zamanaydıı o ya?", lang="tr")
-    elif "dur" in voice:
-        text_to_speech(text="Görüşmek Üzere!!!", lang="tr")
-        exit()
 
 # How to use code basically.
 
